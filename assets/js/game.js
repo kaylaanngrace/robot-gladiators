9c2698d61f71605cf34343ad716f6e219d24bb03
MakWils <kaylaanngrace@gmail.comgit remote add origin git@github.com:kaylaanngrace/horiseon-optimize-accessibility.git>
2022-01-08 03:22:51 -0600

    fixed name prompt bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -61,14 +61,14 @@ var fight = function (enemy) {
 };
 
 //function to start a new game 
-var startGame = function() {
+var startGame = function() { 
   //reset player stats
   playerInfo.reset();
 
   for (var i = 0; i < enemyInfo.length; i++) {
     if(playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));  
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
       
       // pick new enemy to fight based on the index of the enmyNames array
       var pickedEnemyObj = enemyInfo[i];
@@ -166,9 +166,19 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+  var name= "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name
+};
+
 // player info object
 var playerInfo = {
-  name : window.prompt("What is your robot's name?"),
+  name : getPlayerName(),
   health : 100,
   attack : 10,
   money : 10,
